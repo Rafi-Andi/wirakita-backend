8c73fc4d1c2c1c0aeca6d48dc2eea94c1c54c9e1
Rafi-Andi <rafiandiprayitno9528@gmail.com>
2025-09-06 22:47:35 +0700

    endpoint api register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,19 +5,21 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Pest\Plugins\Only;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
         $validatedData = $request->validate([
-            "username" => "string|min:4|max:12|required",
+            "username" => "string|min:4|max:12|required|regex:/^\S*$/|unique:users",
             "fullname" => "string|required",
             "email" => "email|unique:users|required",
             "password" => "string|min:6|required",
             "class" => "string|required",
-        ]);
+        ]); 
 
         try {
             $user = User::create([
@@ -38,6 +40,38 @@ class AuthController extends Controller
                     "username" => $user->username
                 ]
             ], 201);
+        } catch (Exception $e) {
+            return response()->json([
+                "status" => false,
+                "message" => "Gagal register akun",
+                "data" => [
+                    "error" => $e->getMessage()
+                ]
+            ], 500);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        try {
+            if (!Auth::attempt($request->only('email', 'password'))) {
+                return response()->json([
+                    "status" => false,
+                    "message" => "Email / Password salah"
+                ]);
+            }
+
+            $user = User::where('email', $request['email'])->first();
+            $token = $user->createToken('token')->plainTextToken;
+
+            return response()->json([
+                "status" => true,
+                "message" => "berhasil login akun",
+                "data" => [
+                    "token" => $token,
+                    "username" => $user->username
+                ]
+            ], 200);
         } catch (Exception $e) {
             return response()->json([
                 "status" => false,
